9dc56b6cd38f54895f36c7e2b2964cf6b30bf0f2
Aditya Kumar <72186892+AdityaK2608@users.noreply.github.com>
2022-10-15 20:25:13 +0530

    Create sumof2numbers.java

---
+++ Java Programs/Basic/sumof2numbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class sumof2numbers {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);    //System.in is a standard input stream
+        System.out.print("Enter first number- ");
+        int a= in.nextInt();
+        System.out.print("Enter second number- ");
+        int b= in.nextInt();
+        System.out.println(a+b);
+    }
+}
